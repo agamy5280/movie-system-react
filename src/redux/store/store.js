04a70a7b825341b6f3959b53d28a6b7ff85a1931
Mahmoud1055 <mahmoudkamal105d@gmail.com>
2024-06-05 22:57:38 +0300

    add log in com

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesSlice from "./Slices/moviesSlice";
+import usersSlice from "./Slices/usersSlice";
 
 
 
 const store = configureStore({
     reducer:{
         movies: moviesSlice,
+        users : usersSlice,
     }
 })
 
